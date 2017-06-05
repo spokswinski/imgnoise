238d347f1a6493ee2088e567f5b206231e58e272
Ray Heberer <ray.heberer@gmail.com>
2017-06-05 17:59:20 +0200

    started working on loop that applies helper functions to every element in an array

---
+++ R/imgnoise.R
@@ -0,0 +1,10 @@
+imgnoise = function(image, noise_type) {
+  for (row in 1:nrow(image)) {
+    for (col in row:ncol(image)) {
+      if (noise_type == speckle) {
+        image[row,col] = speckle_noise(image[row,col])
+      }
+    }
+  }
+  return(image)
+}
