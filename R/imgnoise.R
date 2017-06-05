86ce3eda68c7b3072a2e9faeb65add8ac6182cd6
Ray Heberer <ray.heberer@gmail.com>
2017-06-05 22:08:28 +0200

    completed function

--- R/imgnoise.R
+++ R/imgnoise.R
@@ -1,11 +1,14 @@
-imgnoise = function(image, noise_type) {
-  for (row in 1:nrow(image)) {
-    for (col in row:ncol(image)) {
-      if (noise_type == speckle) {
-        image[row,col] = speckle_noise(image[row,col])
-      }
-    }
+imgnoise = function(image, noise_type, ...) {
+  if (noise_type == "gaussian") {
+    return(apply(image, 1:length(dim(image)), gaussian_noise, ...))
   }
-  return(image)
+  if (noise_type == "salt_and_pepper") {
+    return(apply(image, 1:length(dim(image)), salt_and_pepper_noise, ...))
+  }
+  if (noise_type == "speckle") {
+    return(apply(image, 1:length(dim(image)), speckle_noise, ...))
+  }
+  if (noise_type == "uniform") {
+    return(apply(image, 1:length(dim(image)), uniform_noise, ...))
+  }
 }
-
